6ad1b643d53ce8ed8bbf19fc4799d60aef6f19cd
ajay0550 <ajaygundu03@gmail.com>
2026-08-08 16:47:15 +0530

    connected to the database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
 const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const pool = require("./db");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req,res)=>{
     res.send("Funds backend running");
 })
 
-const PORT = 5000;
+app.get("/api/test-db", async (req,res)=>{
+    try{
+        const result = await pool.query("SELECT NOW()");
+
+        res.json({
+            message: "Database connected successfully"
+        });
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).json({
+            message: "conenction failed"
+        });
+    }
+});
+
+
+const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`);
