6a20259d760e3827a583cc4ece6cdd6bb0b62a08
ajay0550 <ajaygundu03@gmail.com>
2026-08-08 15:50:01 +0530

    initial setup done

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Funds backend running");
+})
+
+const PORT = 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`);
+});
